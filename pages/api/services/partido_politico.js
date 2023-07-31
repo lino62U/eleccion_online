6e91fe64c4ce4a2f2047831fb41fe8e49a26df47
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-07-31 00:51:37 -0500

    Update partido_politico.js

--- pages/api/services/partido_politico.js
+++ pages/api/services/partido_politico.js
@@ -1,11 +1,15 @@
-import {pool} from "@/ldavis/Data/config/db";
-export default async function handlePartidoPolitico(req,res){
-    try{
-        const [result] = await pool.query("SELECT * FROM partido_politico;");
-        return res.status(200).json(result);
-    }
-    catch (err){
-        res.status(500).json({error: err})
-    }
+import { pool } from "@/ldavis/Data/config/db";
 
+export default async function handlePartidoPolitico(req, res) {
+  try {
+    // Realiza una consulta a la base de datos para obtener los datos de partido_politico
+    const [result] = await pool.query("SELECT * FROM partido_politico;");
+    // Devuelve la respuesta con el resultado de la consulta como un objeto JSON
+    return res.status(200).json(result);
+  } catch (error) {
+    // Si ocurre un error en la consulta a la base de datos, se captura aquí
+    console.error("Error al obtener los datos de partido_politico:", error);
+    // Devuelve una respuesta con un código de estado 500 y un mensaje de error genérico para el cliente
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 }
